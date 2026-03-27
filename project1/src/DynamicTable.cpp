336ddf3de557b398a5f920e0c186c79ba611a9a6
mlody-jano <jasiu.hulboj@gmail.com>
2026-03-27 17:01:08 +0100

    adding the dynamic table implementation

--- project1/src/DynamicTable.cpp
+++ project1/src/DynamicTable.cpp
@@ -1,9 +1,109 @@
 #include "DynamicTable.h"
 
-DynamicTable::DynamicTable()
+DynamicTable::DynamicTable() : table{new int[capacity]}, capacity{100}, size{0} // Initializing the dynamic table with a default capacity of 100
 {
 }
 
 DynamicTable::~DynamicTable()
 {
+    delete[] table;
+}
+
+void DynamicTable::addElementAtBeginning(int element) // Adding an element at the beginning of the table
+{
+    if (size == 0) { // If the table is empty, simply add the element at the first position
+        table[0] = element;
+        size++;
+    }
+    else if (size < capacity) {
+        for (int i = size; i > 0; i--) {
+            table[i] = table[i - 1];
+        }
+        table[0] = element;
+        size++;
+    }
+    checkCapacity();
+}
+
+void DynamicTable::addElementAtEnd(int element) // Adding an element at the end of the table, is 0(1) due to doubling the capacity when table is full
+{
+    table[size] = element;
+    size++;
+    checkCapacity();
+}
+
+void DynamicTable::addElementAtPosition(int element, int position) // Adding an element at a specific position in the table
+{
+    if (position < 0 || position > size) {
+        cerr << "Invalid position. Element not added." << endl;
+        return;
+    }
+
+    if (size == capacity) {
+        checkCapacity();
+    }
+
+    for (int i = size; i > position; i--) { // Shifting elements on the right of position to make room for new element
+        table[i] = table[i - 1];
+    }
+    table[position] = element;
+    size++;
+}
+
+void DynamicTable::deleteElementAtBeginning() // Deleting an element from the beginning of the table
+{
+    if (size == 0) {
+        cerr << "Table is empty. No element to delete." << endl;
+        return;
+    }
+    for (int i = 0; i < size - 1; i++) { // Shifting elements on the right to fill the gap left by deleted element
+        table[i] = table[i + 1];
+    }
+    size--;
+}
+
+void DynamicTable::deleteElementAtEnd()
+{
+    if (size == 0) {
+        cerr << "Table is empty. No element to delete." << endl;
+        return;
+    }
+    table[size - 1] = NULL; // Deleting last element is O(1) due to having the size variable
+    size--;
+}
+
+void DynamicTable::deleteElementAtPosition(int position) // Deleting an element from a specific position in the table
+{
+    if (position < 0 || position >= size) {
+        cerr << "Invalid position. No element deleted." << endl;
+        return;
+    }
+    for (int i = position; i < size - 1; i++) { // Shifting elements on the right of position to fill the gap left by deleted element
+        table[i] = table[i + 1];
+    }
+    size--;
+}
+
+void DynamicTable::searchElement(int element) const // Searching for an element in the table and printing its position if found
+{
+    for (int i = 0; i < size; i++) { // Linear search through the table is 0(n) in the worst case, if we have to navigate through the entire table
+        if (table[i] == element) {
+            cout << "Element " << element << " found at position " << i << "." << endl;
+            return;
+        }
+    }
+    cout << "Element " << element << " not found in the table." << endl;
+}
+
+void DynamicTable::checkCapacity() // Doubling the capacity of the table. Operation is O(n) due to copying size elements to new table
+{
+    if (size == capacity) {
+        int* newTable = new int[capacity * 2];
+        for (int i = 0; i < size; i++) {
+            newTable[i] = table[i];
+        }
+        delete[] table;
+        table = newTable;
+        capacity *= 2;
+    }
 }
--- project1/src/DynamicTable.h
+++ project1/src/DynamicTable.h
@@ -1,13 +1,33 @@
 #ifndef DYNAMICTABLE_H
 #define DYNAMICTABLE_H
 
+#include <iostream>
+
 #pragma once
+
+using namespace std; 
 
 class DynamicTable
 {
 public:
     DynamicTable();
     ~DynamicTable();
+
+    inline int returnSize() const { return size; };
+    inline int returnCapacity() const { return capacity; };
+
+    void addElementAtBeginning(int);
+    void addElementAtEnd(int);
+    void addElementAtPosition(int, int);
+
+    void deleteElementAtBeginning();
+    void deleteElementAtEnd();
+    void deleteElementAtPosition(int);
+
+    void searchElement(int) const;
+
+    void checkCapacity();
+
 private:
     int* table;
     int capacity;
