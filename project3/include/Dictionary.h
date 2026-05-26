b69f9643bef4aa9ee4810fa816c5db6e0775564a
Maciej Polanowski <maciek.polanowski.com@gmail.com>
2026-05-26 20:03:31 +0200

    HasTableList implementation

--- project3/include/Dictionary.h
+++ project3/include/Dictionary.h
@@ -6,12 +6,12 @@ class Dictionary {
 public:
 
     virtual ~Dictionary() = default;
-    Pair<V> insert(const K&, const V&) = 0;
-    Pair<V> remove(const K&) = 0;
-    Pair<V> find(const K&) const = 0;
+    virtual Pair<V> insert(const K&, const V&) = 0;
+    virtual Pair<V> remove(const K&) = 0;
+    virtual Pair<V> find(const K&) const = 0;
 
-    bool exists(const K&) const = 0;
-    int size() const = 0;
-    bool empty() const = 0;
-    
+    virtual bool exists(const K&) const = 0;
+    virtual int size() const = 0;
+    virtual bool empty() const = 0;
+
 };
